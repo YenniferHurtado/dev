dabba3867ca75ad981ff833778ed6236d9701aba
YferHurtado <yennifer.hurtadoarce@gmail.com>
2019-09-15 14:23:55 -0500

    owlCarousel

--- js/main.js
+++ js/main.js
@@ -102,16 +102,17 @@
 	}
 
 	/*--/ Testimonials owl /--*/
-	// $('#testimonial-mf').owlCarousel({
-	// 	margin: 20,
-	// 	autoplay: true,
-	// 	autoplayTimeout: 4000,
-	// 	autoplayHoverPause: true,
-	// 	responsive: {
-	// 		0: {
-	// 			items: 1,
-	// 		}
-	// 	}
-	// });
+	$('#testimonial-mf').owlCarousel({
+		goToFirst: true,
+		margin: 20,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			}
+		}
+	});
 
 })(jQuery);
